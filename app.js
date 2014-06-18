69b8c98197ce68f0a24910d514c60a3f0dfa991a
jayfresh <jonathan@penrosestudio.com>
2014-06-18 17:37:47 +0100

    Changing margin to be a module. Split out the HTML-rendering part into a separate function

--- app.js
+++ app.js
@@ -3,56 +3,86 @@
 */
 var phantom = require('phantom'),
   fs = require('fs'),
+  path = require('path'),
   handlebars = require('handlebars'),
   marked = require('marked'),
-  readOptions = { "encoding": "utf-8" },
-  templateFolder = "template";
+  readOptions = { "encoding": "utf-8" };
 
-/*
- * Read in cover template
- * Render cover + data JSON through handlebars
- * Turn pages.md into HTML
- * Add pages.md to end of cover.html
- * Convert HTML into PDF
- */
 
-phantom.create(function(ph){
-  ph.createPage(function(page) {
-    var templateFilename = __dirname+"/"+templateFolder+"/cover.html";
-    fs.readFile(templateFilename, readOptions, function(err, source) {
-      var template = handlebars.compile(source);
-      
-      var dataJSONFileName = __dirname+"/data.json";
-      fs.readFile(dataJSONFileName, readOptions, function(err, data) {
-    
-        var jsonData = JSON.parse(data);
-        var result = template(jsonData);
-        var filename = __dirname+"/"+templateFolder+"/temp.html";
-        
-        var pagesFileName = __dirname+"/pages.md";
-        
-        fs.readFile(pagesFileName, readOptions, function(err, data) {
-          var pagesHTML = marked(data);
-          var bits = result.split('</body>');
-          // wrap pages HTML in a wrapper and insert before the end of the body
-          bits.splice(1,0,'<div class="wrapper">',pagesHTML,'</div></body>');
-          var htmlForRenderingToPDF = bits.join("");
-
-          fs.writeFile(filename, htmlForRenderingToPDF, function() {
-            page.open(filename, function(status) {
-              page.set('viewportSize', {width:1280,height:1800}, function(result) {
-                page.render('pages.pdf', function(){
-                  console.log('Page Rendered');
-                  ph.exit();
-                  fs.unlink(__dirname+"/"+templateFolder+"/temp.html", function(err) {
-                    console.log('Finished');
-                  });
-                });        
-              });
-            });
+var createPdfFromHTML = function(pdfPath, html, callback) {
+  var tempFilename = path.join(__dirname,"marginTemporaryHTMLFile.html");
+  if(!pdfPath) {
+    pdfPath = path.join(folderPath,"pages.pdf");
+  }
+  if(!html) {
+    html = '';
+  }
+  // write the HTML to a temporary file
+  fs.writeFile(tempFilename, html, function() {
+    // open the temp file in phantom
+    page.open(tempFilename, function(status) {
+      page.set('viewportSize', {width:1280,height:1800}, function(result) {
+        // create the PDF
+        page.render(pdfPath, function(){
+          console.log('Page Rendered');
+          ph.exit();
+          fs.unlink(tempFilename, function(err) {
+            console.log('Finished');
+            if(callback) {
+              callback(err);
+            }
           });
         });
       });
     });
   });
 });
+
+/*
+ * Create a multi-page PDF with a cover template
+ *
+ * Read in cover template
+ * Render cover + data JSON through handlebars
+ * Turn pages.md into HTML
+ * Add pages.md to end of cover.html
+ * Convert HTML into PDF
+ */
+var createPdfFromFolder = function(folderPath, pdfPath, callback) {
+  if(!folderPath) {
+    folderPath = __dirname;
+  }
+  var coverTemplateFilename = path.join(folderPath,"template/cover.html"),
+    dataJSONFileName = path.join(folderPath,"data.json"),
+    pagesFileName = path.join(folderPath,"pages.md");
+    
+  phantom.create(function(ph){
+    ph.createPage(function(page) {
+      // read in the cover template
+      fs.readFile(coverTemplateFilename, readOptions, function(err, source) {
+        var template = handlebars.compile(source);
+        // read in the JSON data for the cover
+        fs.readFile(dataJSONFileName, readOptions, function(err, data) {
+          var jsonData = JSON.parse(data),
+            result = template(jsonData);
+          // read in the markdown document
+          fs.readFile(pagesFileName, readOptions, function(err, data) {
+            // convert markdown to HTML
+            var pagesHTML = marked(data),
+              bits = result.split('</body>'),
+              htmlForRenderingToPDF;
+            // wrap pages HTML in a wrapper and insert before the end of the body
+            bits.splice(1,0,'<div class="wrapper">',pagesHTML,'</div></body>');
+            htmlForRenderingToPDF = bits.join("");
+            // create the PDF
+            createPdfFromHTML(pdfPath, htmlForRenderingToPDF, callback);
+          });
+        });
+      });
+    });
+  });  
+};
+
+module.exports = {
+  createPdfFromHTML: createPdfFromHTML,
+  createPdfFromFolder: createPdfFromFolder
+};
